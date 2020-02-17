d6cf306e7c1d2ef040025077f8acb04c5b6c4a41
Ravi Pavan <ravi.pavan@hp.com>
2020-02-17 14:26:21 +0530

    Save

--- DatabaseCICD/dbo/Tables/Account.sql
+++ DatabaseCICD/dbo/Tables/Account.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Account] (
     [AccountID]        INT          IDENTITY (1, 1) NOT NULL,
-    [AccountName]      VARCHAR (50) NULL,
+    [AccountName]      VARCHAR (50) NULL ,
     [PrimaryContactID] INT          NULL,
     PRIMARY KEY CLUSTERED ([AccountID] ASC),
     CONSTRAINT [FK__Account__Primary__2FCF1A8A] FOREIGN KEY ([PrimaryContactID]) REFERENCES [dbo].[Contact] ([ContactID]) ON DELETE CASCADE
